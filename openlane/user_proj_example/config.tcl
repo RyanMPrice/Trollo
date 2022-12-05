8c40ce3d8ae2924c6648a3bdb0e6b972363d2d61
Ryan Price <91754608+RyanMPrice@users.noreply.github.com>
2022-12-05 11:47:47 -0700

    AllORNothing...

--- openlane/user_proj_example/config.tcl
+++ openlane/user_proj_example/config.tcl
@@ -21,7 +21,19 @@ set ::env(DESIGN_NAME) user_proj_example
 set ::env(VERILOG_FILES) "\
 	$::env(CARAVEL_ROOT)/verilog/rtl/defines.v \
 	$::env(DESIGN_DIR)/../../verilog/rtl/Wishbone/WishboneSlave.v \
+	$::env(DESIGN_DIR)/../../verilog/rtl/defines.v \
+	$::env(DESIGN_DIR)/../../verilog/rtl/DIGOTA/DiffDigota.v \
+	$::env(DESIGN_DIR)/../../verilog/rtl/DIGOTA/DIGOTA.v \
+	$::env(DESIGN_DIR)/../../verilog/rtl/Clocks/clkgate.v \
+	$::env(DESIGN_DIR)/../../verilog/rtl/Clocks/clkmux2.v \
+	$::env(DESIGN_DIR)/../../verilog/rtl/WaveTbl/WavePWM.v \
+	$::env(DESIGN_DIR)/../../verilog/rtl/WaveTbl/WaveTblCosSin.v \ 
+	$::env(DESIGN_DIR)/../../verilog/rtl/WaveTbl/WaveTblSig.v \ 
+	$::env(DESIGN_DIR)/../../verilog/rtl/BinMult/BinMultiplier.v \
 	$::env(DESIGN_DIR)/../../verilog/rtl/user_proj_example.v"
+
+# Latch mapping
+set ::env(SYNTH_LATCH_MAP) "$::env(DESIGN_DIR)/../../verilog/rtl/yosysStdCell/latch_map.v"
 
 set ::env(DESIGN_IS_CORE) 0
 
@@ -29,8 +41,8 @@ set ::env(CLOCK_PORT) "wb_clk_i"
 set ::env(CLOCK_NET) "clk"
 set ::env(CLOCK_PERIOD) "24.0"
 
-#set ::env(FP_SIZING) absolute
-#set ::env(DIE_AREA) "0 0 900 600"
+set ::env(FP_SIZING) absolute
+set ::env(DIE_AREA) "0 0 1000 1000"
 
 set ::env(FP_PIN_ORDER_CFG) $::env(DESIGN_DIR)/pin_order.cfg
 
@@ -40,47 +52,6 @@ set ::env(PL_TARGET_DENSITY) 0.50
 set ::env(FP_CORE_UTIL) 40
 
 set ::env(SYNTH_MAX_FANOUT) 4
-
-### Macro PDN Connections
-set ::env(FP_PDN_MACRO_HOOKS) "\
-	mprj vdd vss vdd vss"
-
-### Black-box verilog and views
-set ::env(VERILOG_FILES_BLACKBOX) "\
-	$::env(DESIGN_DIR)/../../verilog/rtl/defines.v \
-	$::env(DESIGN_DIR)/../../verilog/rtl/DIGOTA/DiffDigota.v \
-	$::env(DESIGN_DIR)/../../verilog/rtl/DIGOTA/DIGOTA.v \
-	$::env(DESIGN_DIR)/../../verilog/rtl/Clocks/clkgate.v \
-	$::env(DESIGN_DIR)/../../verilog/rtl/Clocks/clkmux2.v \
-	$::env(DESIGN_DIR)/../../verilog/rtl/WaveTbl/WavePWM.v"
-
-set ::env(EXTRA_LEFS) "\
-	$::env(DESIGN_DIR)/../../lef/DiffDigota.lef \
-	$::env(DESIGN_DIR)/../../lef/DIGOTA.lef \
-	$::env(DESIGN_DIR)/../../lef/clkgate.lef \
-	$::env(DESIGN_DIR)/../../lef/clkmux2.lef \
-	$::env(DESIGN_DIR)/../../lef/WavePWM.lef"
-
-set ::env(EXTRA_GDS_FILES) "\
-	$::env(DESIGN_DIR)/../../gds/DiffDigota.gds \
-	$::env(DESIGN_DIR)/../../gds/DIGOTA.gds \
-	$::env(DESIGN_DIR)/../../gds/clkgate.gds \
-	$::env(DESIGN_DIR)/../../gds/clkmux2.gds \
-	$::env(DESIGN_DIR)/../../gds/WavePWM.gds"
-
-set ::env(EXTRA_LIBS) "\
-    $::env(DESIGN_DIR)/../../lib/DiffDigota.lib \
-    $::env(DESIGN_DIR)/../../lib/DIGOTA.lib \
-    $::env(DESIGN_DIR)/../../lib/clkgate.lib\
-    $::env(DESIGN_DIR)/../../lib/clkmux2.lib\
-    $::env(DESIGN_DIR)/../../lib/WavePWM.lib"
-
-set ::env(RT_MAX_LAYER) {Metal4}
-
-# disable pdn check nodes becuase it hangs with multiple power domains.
-# any issue with pdn connections will be flagged with LVS so it is not a critical check.
-set ::env(FP_PDN_CHECK_NODES) 0
-
 
 # Maximum layer used for routing is metal 4.
 # This is because this macro will be inserted in a top level (user_project_wrapper) 
